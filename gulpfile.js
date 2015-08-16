251bc475f35e03ecd7061ff456cf10807b6454cd
koh110 <kohta110@gmail.com>
2015-08-17 00:34:09 +0900

    add bower

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var mainBowerFiles = require('main-bower-files');
 var browserSync =require('browser-sync');
 var notifier = require('node-notifier');
 
+// 設定
 var config = {
   server : {
     port: 8282
+  },
+  html: {
+    injectTarget: './app/index.html'
   },
   js: {
     output: {
@@ -36,6 +41,7 @@ var config = {
   }
 };
 
+// エラー時のnotify表示
 var notify = function(taskName, error) {
   var title = '[task]' + taskName + ' ' + error.plugin;
   var errorMsg = 'error: ' + error.message;
@@ -47,6 +53,7 @@ var notify = function(taskName, error) {
   });
 };
 
+// サーバ起動
 gulp.task('server', function() {
   browserSync({
     port: config.server.port,
@@ -57,10 +64,13 @@ gulp.task('server', function() {
   });
 });
 
+// サーバ再起動
 gulp.task('reloadServer', function () {
   browserSync.reload();
 });
 
+// css系処理
+// css連結 -> autoprefixer
 gulp.task('css', function() {
   return gulp.src(config.css.files)
     .pipe($.plumber({
@@ -79,6 +89,8 @@ gulp.task('css', function() {
     .pipe(gulp.dest(config.css.output.directory));
 });
 
+// js系処理
+// es6からes5への変換 -> js連結
 gulp.task('js', ['lint'], function() {
   return gulp.src(config.js.files)
     .pipe($.plumber({
@@ -92,6 +104,7 @@ gulp.task('js', ['lint'], function() {
     .pipe(gulp.dest(config.js.output.directory));
 });
 
+// jsのlint処理
 gulp.task('lint', function() {
   return gulp.src(config.js.files)
     .pipe($.plumber({
@@ -105,12 +118,22 @@ gulp.task('lint', function() {
     .pipe($.plumber.stop());
 });
 
-gulp.task('build', ['js', 'css'], function() {});
+// bowerで取得したファイルをindex.htmlに挿入
+gulp.task('inject', function() {
+  return gulp.src(config.html.injectTarget)
+    .pipe($.inject(gulp.src(mainBowerFiles()), {
+      name: 'inject'
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
+// jsとcssのビルド処理
+gulp.task('build', ['js', 'inject', 'css'], function() {});
 
 gulp.task('watch', function() {
-  gulp.watch('app/index.html', ['reloadServer']);
+  gulp.watch('./app/index.html', ['reloadServer']);
   gulp.watch(config.js.files, ['js', 'reloadServer']);
   gulp.watch(config.css.files, ['css', 'reloadServer']);
 });
 
-gulp.task('default', ['watch', 'server']);
+gulp.task('default', ['build', 'watch', 'server']);
