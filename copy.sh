d9d41973a83b8188a26753c7614d63f3bec0910d
koh110 <kohta110@gmail.com>
2015-08-27 01:02:09 +0900

    add copy.sh

---
+++ copy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# copy用シェルスクリプト
+# ./copy.sh /path/to/project
+
+if [ $# -ne 1 ]; then
+  echo 'args error'
+  exit 1
+fi
+
+if [ -e $1 ]; then
+  echo 'directory exist'
+else
+  echo 'directory not exist'
+  exit 1
+fi
+
+dirname=`dirname $0`
+
+rsync -ave --exclude '.DS_Store' --exclude '.git' --exclude 'copy.sh' --exclude 'README.md' --exclude 'node_modules' --exclude 'app/components' ${dirname}/ ${1}
